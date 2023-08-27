800602510d08e10ee062d9a5dd966a8a6ef43997
Aman Patel <patelpatelaman00@gmail.com>
2023-08-27 20:40:16 +0530

    Add register and login functionality

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ Route::get('/logout', function () {
     return redirect('login');
 });
 
+Route::view('/register','register');
 Route::post("/login", [UserController::class, 'login']);
+Route::post("/register", [UserController::class, 'register']);
 Route::get("/", [ProductController::class, 'index']);
 Route::get("detail/{id}", [ProductController::class, 'detail']);
 Route::post("add_to_cart", [ProductController::class, 'addToCart']);
